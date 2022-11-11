81716f6bf75adf4d7d88c94d28a7fc2467f7f179
Martin <mrtn.karsten@gmail.com>
2022-11-11 07:29:27 +0100

    library setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  // If our .vue files have a style, it will be compiled as a single `.css` file under /dist.
+  plugins: [Vue({ style: { filename: "style.css" } })],
+
+  build: {
+    // Output compiled files to /dist.
+    outDir: "./dist",
+    lib: {
+      // Set the entry point (file that contains our components exported).
+      entry: resolve(__dirname, "src/index.ts"),
+      // Name of the library.
+      name: "vue3-drag-resize",
+      // We are building for CJS and ESM, use a function to rename automatically files.
+      // Example: vue3-drag-resize.esm.js
+      fileName: (format) => `${"vue3-drag-resize"}.${format}.js`,
+    },
+    rollupOptions: {
+      // Vue is provided by the parent project, don't compile Vue source-code inside our library.
+      external: ["vue"],
+      output: { globals: { vue: "Vue" } },
+    },
+  },
+});
